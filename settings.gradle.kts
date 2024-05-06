68f0795ea8fd46ad5cb174eabf8087eae0e3aff7
Muhammed Fatih Tadık <fatihtadk@gmail.com>
2024-05-06 23:16:13 +0300

    Groovy'den kts'ye gradle güncellemesi

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = 'https://jitpack.io' }
+        maven("https://jitpack.io")
     }
 }
 
 rootProject.name = "NewsApp"
-include ':app'
+include(":app")
